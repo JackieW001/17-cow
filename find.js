058e5d08a1772baec8e1738bc7d609827dcf90f4
Xing Tao Shi <xshi@stuy.edu>
2017-12-12 14:01:09 -0500

    broken

--- find.js
+++ find.js
@@ -13,12 +13,22 @@ console.log( "box width: " + boxWidth );
 
 //calculate distance between current mouse pos and target
 var distance = function (x0, y0, x1, y1) {
-  /* YOUR IMPLEMENTATION */
+    /* YOUR IMPLEMENTATION */
+    var retVal = Math.sqrt(Math.pow((x0-x1), 2) + Math.pow((y0-y1), 2));
+    return retVal;
 };
 
 
 var findIt = function(e) {
-  /* YOUR IMPLEMENTATION */
+    /* YOUR IMPLEMENTATION */
+    // console.log(e.clientX);
+    // console.log(e.clientY);
+    // console.log(targetX);
+    // console.log(targetY);
+    var dist = distance(e.clientX, e.clientY, targetX, targetY);
+    console.log(dist);
+    var hexString = Math.floor(dist).toString(16);
+    box.setAttribute("color", "#" + hexString);
 };
 
 /*
